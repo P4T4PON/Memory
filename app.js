0766ecca186b219d003d23b9fa8ad897ce3f310b
P4T4PON <r4ptor1@onet.pl>
2019-10-15 17:21:54 +0200

    modes

--- app.js
+++ app.js
@@ -1,6 +1,43 @@
 const tile = document.querySelector('.game-board');
 const btn = document.querySelector('.game-start');
 
+let modeVal = document.getElementsByTagName('option');
+//console.log(modeVal[0].label);
+let x = null;
+
+document.addEventListener(
+  'input',
+  function(event) {
+    if (event.target.id !== 'mod') return;
+
+    if (event.target.value === 'easy') {
+      x = 0;
+    }
+    if (event.target.value === 'medium') {
+      x = 1;
+    }
+    if (event.target.value === 'hard') {
+      x = 2;
+    }
+  },
+  false
+);
+
+console.log(x);
+/*
+modeVal = modeVal[x].label;
+
+if (modeVal[x] === 'Easy') {
+  modeVal = 4;
+} else if (modeVal[x] === 'Medium') {
+  modeVal = 8;
+} else if (modeVal[x] === 'Hard') {
+  modeVal = 16;
+} else {
+  alert('error');
+}
+//console.log(modeVal);
+*/
 const max = 8;
 const min = 1;
 
@@ -10,7 +47,9 @@ tl.innerHTML =
   '<img src=title_' + Math.floor(Math.random() * (max - min) + min) + '.png >';
 
 const click = function() {
-  document.querySelector('.game-board').appendChild(tl);
+  for (i = 0; i <= modeVal; i++) {
+    document.querySelector('.game-board').appendChild(tl);
+  }
 };
 
 btn.addEventListener('click', click);
