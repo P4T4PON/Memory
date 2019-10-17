582aec4130a7ee06139d733e09fd23da869c0b02
P4T4PON <r4ptor1@onet.pl>
2019-10-17 15:48:00 +0200

    before znak's zapytak's

--- app.js
+++ app.js
@@ -8,9 +8,25 @@ const min = 1;
 var x = undefined;
 var y = undefined;
 let arr = [];
-let til = [1, 2, 3, 4, 5, 6, 7, 8];
 
 let mat = Math.floor(Math.random() * (max - min) + min);
+
+function shuffle(arr) {
+  var currentIndex = arr.length,
+    temporaryValue,
+    randomIndex;
+
+  while (0 !== currentIndex) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    temporaryValue = arr[currentIndex];
+    arr[currentIndex] = arr[randomIndex];
+    arr[randomIndex] = temporaryValue;
+  }
+
+  return arr;
+}
 
 document.addEventListener(
   'input',
@@ -33,24 +49,60 @@ document.addEventListener(
 const click = function() {
   if (modeVal[x].label === 'Easy') {
     y = 4;
+    for (let i = 0; i < y / 2; i++) {
+      let a = tiles.splice(
+        Math.floor(Math.random() * (tiles.length - 1) + 1),
+        1
+      )[0];
+      arr.push(a);
+      arr.push(a);
+      shuffle(arr);
+    }
+    for (let i = 0; i < y; i++) {
+      var tl = document.createElement('div');
+      tl.className = 'etile';
+      tl.innerHTML = `<img src=${arr[i]} />`;
+      console.log(tl.innerHTML);
+      tile.appendChild(tl);
+    }
   } else if (modeVal[x].label === 'Medium') {
     y = 8;
+    for (let i = 0; i < y / 2; i++) {
+      let a = tiles.splice(
+        Math.floor(Math.random() * (tiles.length - 1) + 1),
+        1
+      )[0];
+      arr.push(a);
+      arr.push(a);
+      shuffle(arr);
+    }
+    for (let i = 0; i < y; i++) {
+      var tl = document.createElement('div');
+      tl.className = 'mtile';
+      tl.innerHTML = `<img src=${arr[i]} />`;
+      console.log(tl.innerHTML);
+      tile.appendChild(tl);
+    }
   } else if (modeVal[x].label === 'Hard') {
     y = 16;
+    for (let i = 0; i < y / 2; i++) {
+      let a = tiles.splice(
+        Math.floor(Math.random() * (tiles.length - 1) + 1),
+        1
+      )[0];
+      arr.push(a);
+      arr.push(a);
+      shuffle(arr);
+    }
+    for (let i = 0; i < y; i++) {
+      var tl = document.createElement('div');
+      tl.className = 'htile';
+      tl.innerHTML = `<img src=${arr[i]} />`;
+      console.log(arr[i]);
+      tile.appendChild(tl);
+    }
   } else {
     alert('error');
-  }
-  for (let i = 0; i < y; i++) {
-    /*
-    let tl = document.createElement('div');
-    tl.className = 'tile';
-    tl.innerHTML = '<img src=title_' + mat + '.png >';
-    tile.appendChild(tl);
-    mat = Math.floor(Math.random() * (max - min) + min);
-    */
-    let a = til.splice(Math.floor(Math.random() * (til.length - 1) + 1));
-    arr.push(a);
-    arr.push(a);
   }
 };
 
@@ -64,5 +116,7 @@ let tiles = [
   'tile_5.png',
   'tile_6.png',
   'tile_7.png',
-  'tile_8.png'
+  'tile_8.png',
+  'tile_9.png',
+  'tile_10.png'
 ];
