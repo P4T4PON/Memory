72acd11da5d0090af394313e2d41e64a4dcb824b
P4T4PON <r4ptor1@onet.pl>
2019-10-20 17:20:13 +0200

    almost done

--- app.js
+++ app.js
@@ -5,8 +5,12 @@ let modeVal = document.getElementsByTagName('option');
 const max = 8;
 const min = 1;
 
+var a = undefined;
+var m = undefined;
 var x = undefined;
-var y = undefined;
+
+let tilesChecked = [];
+
 let arr = [];
 
 let mat = Math.floor(Math.random() * (max - min) + min);
@@ -49,61 +53,59 @@ document.addEventListener(
 const click = function() {
   if (modeVal[x].label === 'Easy') {
     y = 4;
-    for (let i = 0; i < y / 2; i++) {
-      let a = tiles.splice(
-        Math.floor(Math.random() * (tiles.length - 1) + 1),
-        1
-      )[0];
-      arr.push(a);
-      arr.push(a);
-      shuffle(arr);
-    }
-    for (let i = 0; i < y; i++) {
-      var tl = document.createElement('div');
-      tl.className = 'etile';
-      tl.innerHTML = `<img src=${arr[i]} />`;
-      console.log(tl.innerHTML);
-      tile.appendChild(tl);
-    }
+    m = 'e';
   } else if (modeVal[x].label === 'Medium') {
     y = 8;
-    for (let i = 0; i < y / 2; i++) {
-      let a = tiles.splice(
-        Math.floor(Math.random() * (tiles.length - 1) + 1),
-        1
-      )[0];
-      arr.push(a);
-      arr.push(a);
-      shuffle(arr);
-    }
-    for (let i = 0; i < y; i++) {
-      var tl = document.createElement('div');
-      tl.className = 'mtile';
-      tl.innerHTML = `<img src=${arr[i]} />`;
-      console.log(tl.innerHTML);
-      tile.appendChild(tl);
-    }
+    m = 'm';
   } else if (modeVal[x].label === 'Hard') {
     y = 16;
-    for (let i = 0; i < y / 2; i++) {
-      let a = tiles.splice(
-        Math.floor(Math.random() * (tiles.length - 1) + 1),
-        1
-      )[0];
-      arr.push(a);
-      arr.push(a);
-      shuffle(arr);
-    }
-    for (let i = 0; i < y; i++) {
-      var tl = document.createElement('div');
-      tl.className = 'htile';
-      tl.innerHTML = `<img src=${arr[i]} />`;
-      console.log(arr[i]);
-      tile.appendChild(tl);
-    }
+    m = 'h';
   } else {
     alert('error');
   }
+  for (let i = 0; i < y / 2; i++) {
+    let a = tiles.splice(
+      Math.floor(Math.random() * (tiles.length - 1) + 1),
+      1
+    )[0];
+    arr.push(a);
+    arr.push(a);
+    shuffle(arr);
+  }
+  for (let i = 0; i < y; i++) {
+    var tl = document.createElement('div');
+    tl.className = `${m}tile`;
+    tl.innerHTML = `<img src=${arr[i]} class="display-none" id='a'/> <img src=tile.png class="display" id='a' >`;
+    a = i;
+    tl.id = i;
+    tile.appendChild(tl);
+
+    tl.addEventListener('click', clicked);
+  }
+};
+
+const clicked = function(e) {
+  e.target.className = 'display-none';
+  e.target.previousElementSibling.className = 'display';
+  tilesChecked.push(e.target.previousElementSibling.src);
+  console.log(tilesChecked[0]);
+  console.log(tilesChecked[1]);
+  if (tilesChecked.length === 2)
+    if (tilesChecked[0] === tilesChecked[1]) {
+      deleteTiles();
+      //e.target.previousElementSibling = null;
+      //tilesChecked[0].remove();
+      //tilesChecked[1].remove();
+      //console.log(tilesChecked);
+      //tilesChecked = [];
+    }
+};
+
+let deleteTiles = function() {
+  tilesChecked[0].remove();
+  tilesChecked[1].remove();
+  console.log(tilesChecked);
+  tilesChecked = [];
 };
 
 btn.addEventListener('click', click);
