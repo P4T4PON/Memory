6612c29c79105a1b7c20328cf669ef147240bd55
P4T4PON <r4ptor1@onet.pl>
2019-10-23 20:49:39 +0200

    commments added properties changed

--- app.js
+++ app.js
@@ -1,26 +1,68 @@
-const tile = document.querySelector('.game-board');
-const btn = document.querySelector('.game-start');
-let modeVal = document.getElementsByTagName('option');
-let option = document.getElementById('#o1');
+// Grabing things
+const gameBoard = document.querySelector('.game-board');
+const PlayButton = document.querySelector('.game-start');
+let levelOfDifficulty = document.getElementsByTagName('option');
 let score = document.querySelector('.game-score');
 
+// img array
+let tiles = [
+  'tile_1.png',
+  'tile_2.png',
+  'tile_3.png',
+  'tile_4.png',
+  'tile_5.png',
+  'tile_6.png',
+  'tile_7.png',
+  'tile_8.png',
+  'tile_9.png',
+  'tile_10.png'
+];
+
+//STARTING VALUES
+
+// math.random max and min value
 const max = 8;
 const min = 1;
 
+// pairs of images found
 let tilePairs = 0;
+
+// how many tiles to create
 let tileCount = 0;
+
+// how many moves did we do
 let moveCount = null;
 
-var a = undefined;
-var m = undefined;
-var x = 0;
+// return's different value for each level of difficulty we chose
+var gameMode = undefined;
 
+// return's index of option
+var optionIndex = 0;
+
+// if ypu click on tile push the target to this array
 let tilesChecked = [];
 
+// array for images gruped in pairs
 let arr = [];
 
+// returns random number beetwen 1 and 8
 let mat = Math.floor(Math.random() * (max - min) + min);
 
+// FUNCTIONS
+
+// when you win set select value to "pick one"
+function select(selectId, optionValToSelect) {
+  var selectElement = document.getElementById(selectId);
+  var selectOptions = selectElement.options;
+  for (var opt, j = 0; (opt = selectOptions[j]); j++) {
+    if (opt.value == optionValToSelect) {
+      selectElement.selectedIndex = j;
+      break;
+    }
+  }
+}
+
+// shuffle images in array
 function shuffle(arr) {
   var currentIndex = arr.length,
     temporaryValue,
@@ -38,42 +80,39 @@ function shuffle(arr) {
   return arr;
 }
 
+// when you select difficulty level change these values
 document.addEventListener(
   'input',
   function(e) {
     if (e.target.value === 'easy') {
-      x = 1;
+      optionIndex = 1;
       tileCount = 4;
     } else if (e.target.value === 'medium') {
-      x = 2;
+      optionIndex = 2;
       tileCount = 8;
     } else if (e.target.value === 'hard') {
-      x = 3;
+      optionIndex = 3;
       tileCount = 16;
     } else {
-      x = 0;
+      optionIndex = 0;
     }
   },
   false
 );
 
-//modeVal = modeVal[x].label;
-
+// when you click Play change these values
 const click = function() {
-  if (modeVal[x].label === 'Easy') {
-    y = 4;
-    m = 'e';
-  } else if (modeVal[x].label === 'Medium') {
-    y = 8;
-    m = 'm';
-  } else if (modeVal[x].label === 'Hard') {
-    y = 16;
-    m = 'h';
+  if (levelOfDifficulty[optionIndex].label === 'Easy') {
+    divClass = 'e';
+  } else if (levelOfDifficulty[optionIndex].label === 'Medium') {
+    divClass = 'm';
+  } else if (levelOfDifficulty[optionIndex].label === 'Hard') {
+    divClass = 'h';
   } else {
-    y = 0;
     alert('pick one!!!');
   }
-  for (let i = 0; i < y / 2; i++) {
+  // pick random images in pairs from "tiles" and push them to "arr" array then shuffle them
+  for (let i = 0; i < tileCount / 2; i++) {
     let a = tiles.splice(
       Math.floor(Math.random() * (tiles.length - 1) + 1),
       1
@@ -82,26 +121,30 @@ const click = function() {
     arr.push(a);
     shuffle(arr);
   }
-  for (let i = 0; i < y; i++) {
-    var tl = document.createElement('div');
-    tl.className = `${m}tile`;
-    tl.innerHTML = `<img src=${arr[i]} class="display-none" id='a'/> <img src=tile.png class="display" id='a' >`;
-    a = i;
-    tl.id = i;
-    tile.appendChild(tl);
+  // create div's give them class and put images from arr in them
+  for (let i = 0; i < tileCount; i++) {
+    var createDiv = document.createElement('div');
+    // takes value from select and returns class for div's
+    createDiv.className = `${divClass}tile`;
+    // change empty div for div with hidden image and default tile image
+    createDiv.innerHTML = `<img src=${arr[i]} class="display-none"/> <img src=tile.png class="display">`;
+    createDiv.id = i;
+    gameBoard.appendChild(createDiv);
 
-    tl.addEventListener('click', clicked);
+    createDiv.addEventListener('click', changeStylesOfImages);
   }
 };
 
-const clicked = function(e) {
+// when you click image change its class
+const changeStylesOfImages = function(e) {
   e.target.className = 'display-none';
   e.target.previousElementSibling.className = 'display';
   tilesChecked.push(e.target);
+  //if array "tilesChecked" has 2 elements in it
   if (tilesChecked.length === 2) {
     moveCount++;
-    //console.log(score);
     score.innerText = moveCount;
+    // change classes of images in div
     let resetTiles = function() {
       tilesChecked[0].previousElementSibling.className = 'display-none';
       tilesChecked[0].className = 'display';
@@ -109,67 +152,32 @@ const clicked = function(e) {
       tilesChecked[1].className = 'display';
       tilesChecked = [];
     };
+    // delete elements from "tilesChecked" array
     let deleteTiles = function() {
       tilesChecked[0].previousElementSibling.remove();
       tilesChecked[1].previousElementSibling.remove();
       console.log(tilesChecked);
       tilesChecked = [];
       tilePairs++;
+      //if there are no more elements on game board end game
       if (tilePairs >= tileCount / 2) {
         alert('Gratki');
-
-        //tile.innerHTML = '';
         select('mod', 'Pick mode');
         location.reload();
-
-        //if (tile.innerHTML === '') {
-        //y = undefined;
-        // m = undefined;
-        //}
       }
     };
     if (
+      // if we find 2 the same images
       tilesChecked[0].previousElementSibling.src ===
       tilesChecked[1].previousElementSibling.src
     ) {
-      console.log('deleteTiles');
       setTimeout(deleteTiles, 500);
-      //e.target.previousElementSibling = null;
-      //tilesChecked[0].remove();
-      //tilesChecked[1].remove();
-      //console.log(tilesChecked);
-      //tilesChecked = [];
     } else {
-      console.log('resetTiles');
+      // if we didn't found 2 the same images
       setTimeout(resetTiles, 500);
-      console.log(tilesChecked);
-      //resetTiles();
     }
   }
 };
 
-btn.addEventListener('click', click);
-
-let tiles = [
-  'tile_1.png',
-  'tile_2.png',
-  'tile_3.png',
-  'tile_4.png',
-  'tile_5.png',
-  'tile_6.png',
-  'tile_7.png',
-  'tile_8.png',
-  'tile_9.png',
-  'tile_10.png'
-];
-
-function select(selectId, optionValToSelect) {
-  var selectElement = document.getElementById(selectId);
-  var selectOptions = selectElement.options;
-  for (var opt, j = 0; (opt = selectOptions[j]); j++) {
-    if (opt.value == optionValToSelect) {
-      selectElement.selectedIndex = j;
-      break;
-    }
-  }
-}
+// give button "Play" functionality when clicked
+PlayButton.addEventListener('click', click);
